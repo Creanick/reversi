85565f7f2f5bdfbe4e686177a33f21338934eaf5
Manick <manick@Manicks-MacBook-Air.local>
2019-08-10 15:20:36 +0530

    basic position created

---
+++ src/Position/Position.test.ts
@@ -0,0 +1,28 @@
+import Position from './Position';
+const boardSize = 4;
+it('should throw for 0 position ', (): void => {
+    expect((): void => {
+        new Position(0, boardSize);
+    }).toThrow(Position.ErrorMessages.positionError);
+});
+it('should not throw error for right positions', (): void => {
+    for (let i = 1; i <= boardSize * boardSize; i++) {
+        expect((): void => {
+            new Position(i, boardSize);
+        }).not.toThrow();
+    }
+});
+
+it('should throw error for wrong positions', (): void => {
+    expect((): void => {
+        new Position(boardSize * boardSize + 1, boardSize);
+    }).toThrow();
+    expect((): void => {
+        new Position(boardSize * boardSize + 2, boardSize);
+    }).toThrow(Position.ErrorMessages.positionError);
+});
+it('should throw error for negative position', (): void => {
+    expect((): void => {
+        new Position(-34, boardSize);
+    }).toThrow(Position.ErrorMessages.positionError);
+});
---
+++ src/Position/Position.ts
@@ -0,0 +1,28 @@
+import conditionError from '../utils/conditionError';
+enum ErrorsMessages {
+    positionError = 'Position is not inside the board',
+}
+enum Directions {
+    left,
+    right,
+    top,
+    down,
+}
+class Position {
+    public position: number;
+    public size: number;
+    public static ErrorMessages: typeof ErrorsMessages;
+    public constructor(position: number, size: number) {
+        conditionError(1 <= position && position <= size * size, ErrorsMessages.positionError);
+        this.position = position;
+        this.size = size;
+    }
+    public isEdge(direction: Directions): boolean {
+        if (direction === Directions.top) {
+            return 1 <= this.position && this.position <= this.size;
+        }
+        return true;
+    }
+}
+Position.ErrorMessages = ErrorsMessages;
+export = Position;
