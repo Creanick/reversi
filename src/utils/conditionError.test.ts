e24ddcb895fcc1a0e24c30fe6c9242170279d421
Manick <manick@Manicks-MacBook-Air.local>
2019-08-10 15:08:50 +0530

    condition test file added

---
+++ src/utils/conditionError.test.ts
@@ -0,0 +1,11 @@
+import conditionError from './conditionError';
+
+it('should throw error', (): void => {
+    expect((): void => conditionError(false, 'throw error')).toThrow('throw error');
+    expect((): void => conditionError(false, 'throw error')).toThrow(Error);
+});
+it('should not throw any error', (): void => {
+    (): void => {
+        expect((): void => conditionError(true, 'dasfdsa')).not.toThrow();
+    };
+});
