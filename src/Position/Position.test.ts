45e59bc322bae2ac066357681efd589389c1a236
Manick <manick@Manicks-MacBook-Air.local>
2019-08-11 16:23:23 +0530

    position isEdge hasEdge hasCorner method setup and test

--- src/Position/Position.test.ts
+++ src/Position/Position.test.ts
@@ -1,29 +1,106 @@
-import Position from './Position';
+import Position, { ErrorMessages, Edges, Corners } from './Position';
 
-const boardSize = 4;
-it('should throw for 0 position ', (): void => {
-    expect((): void => {
-        new Position(0, boardSize);
-    }).toThrow(Position.ErrorMessages.positionError);
-});
-it('should not throw error for right positions', (): void => {
-    for (let i = 1; i <= boardSize * boardSize; i += 1) {
-        expect((): void => {
-            new Position(i, boardSize);
-        }).not.toThrow();
-    }
-});
+function testBegin(size: number): void {
+    describe(`Test for board size ${size}`, (): void => {
+        it('size should not be greater than 1', (): void => {
+            expect((): void => {
+                new Position(1, 1);
+            }).toThrow(ErrorMessages.sizeError);
+        });
+        if (size < 2) return;
+        it('size should be greater than 1', (): void => {
+            expect((): void => {
+                new Position(1, size);
+            }).not.toThrow();
+        });
+        it('should throw for 0 position ', (): void => {
+            expect((): void => {
+                new Position(0, size);
+            }).toThrow(ErrorMessages.positionError);
+        });
+        it('should not throw error for right positions', (): void => {
+            for (let i = 1; i <= size * size; i += 1) {
+                expect((): void => {
+                    new Position(i, size);
+                }).not.toThrow();
+            }
+        });
 
-it('should throw error for wrong positions', (): void => {
-    expect((): void => {
-        new Position(boardSize * boardSize + 1, boardSize);
-    }).toThrow();
-    expect((): void => {
-        new Position(boardSize * boardSize + 2, boardSize);
-    }).toThrow(Position.ErrorMessages.positionError);
-});
-it('should throw error for negative position', (): void => {
-    expect((): void => {
-        new Position(-34, boardSize);
-    }).toThrow(Position.ErrorMessages.positionError);
-});
+        it('should throw error for wrong positions', (): void => {
+            expect((): void => {
+                new Position(size * size + 1, size);
+            }).toThrow();
+            expect((): void => {
+                new Position(size * size + 2, size);
+            }).toThrow(ErrorMessages.positionError);
+        });
+        it('should throw error for negative position', (): void => {
+            expect((): void => {
+                new Position(-34, size);
+            }).toThrow(ErrorMessages.positionError);
+        });
+        it('should be true for right top edge', (): void => {
+            for (let i = 1; i <= size; i += 1) {
+                const p1 = new Position(i, size);
+                expect(p1.isEdge(Edges.top)).toBeTruthy();
+                expect(p1.hasEdge(Edges.top)).toBeFalsy();
+                expect(p1.hasCorner(Corners.topLeft)).toBeFalsy();
+                expect(p1.hasCorner(Corners.topRight)).toBeFalsy();
+            }
+        });
+        it('should be false for wrong top edge', (): void => {
+            for (let i = size + 1; i <= size ** 2; i += 1) {
+                const p = new Position(i, size);
+                expect(p.isEdge(Edges.top)).toBeFalsy();
+                expect(p.hasEdge(Edges.top)).toBeTruthy();
+            }
+        });
+        it('should be true for right left edge', (): void => {
+            for (let i = size + 1; i < size ** 2; i += size) {
+                const p = new Position(i, size);
+                expect(p.isEdge(Edges.left)).toBeTruthy();
+                expect(p.hasEdge(Edges.left)).toBeFalsy();
+                expect(p.hasCorner(Corners.topLeft)).toBeFalsy();
+                expect(p.hasCorner(Corners.bottomLeft)).toBeFalsy();
+            }
+        });
+        it('should be false for wrong left edge', (): void => {
+            const p = new Position(size + 2, size);
+            expect(p.isEdge(Edges.left)).toBeFalsy();
+            expect(p.hasEdge(Edges.left)).toBeTruthy();
+        });
+        it('should be true for right right edge', (): void => {
+            for (let i = size; i <= size ** 2; i += size) {
+                const p = new Position(i, size);
+                expect(p.isEdge(Edges.right)).toBeTruthy();
+                expect(p.hasEdge(Edges.right)).toBeFalsy();
+                expect(p.hasCorner(Corners.topRight)).toBeFalsy();
+                expect(p.hasCorner(Corners.bottomRight)).toBeFalsy();
+            }
+        });
+        it('should be false for wrong right edge', (): void => {
+            const p = new Position(size - 1, size);
+            expect(p.isEdge(Edges.right)).toBeFalsy();
+            expect(p.hasEdge(Edges.right)).toBeTruthy();
+        });
+        it('should be true for right down edge', (): void => {
+            for (let i = ((size - 1) * size) + 1; i <= size ** 2; i += 1) {
+                const p = new Position(i, size);
+                expect(p.isEdge(Edges.bottom)).toBeTruthy();
+                expect(p.hasEdge(Edges.bottom)).toBeFalsy();
+                expect(p.hasCorner(Corners.bottomLeft)).toBeFalsy();
+                expect(p.hasCorner(Corners.bottomRight)).toBeFalsy();
+            }
+        });
+        it('should be false for wrong bottom edge', (): void => {
+            const p = new Position((size - 1) * size, size);
+            expect(p.isEdge(Edges.bottom)).toBeFalsy();
+            expect(p.hasEdge(Edges.bottom)).toBeTruthy();
+        });
+    });
+}
+
+testBegin(4);
+testBegin(2);
+testBegin(3);
+testBegin(1);
--- src/Position/Position.ts
+++ src/Position/Position.ts
@@ -1,33 +1,68 @@
 import conditionError from '../utils/conditionError';
 
-enum ErrorsMessages {
+export enum ErrorMessages {
     positionError = 'Position is not inside the board',
+    sizeError='Size should be greater than 1'
 }
-enum Directions {
+export enum Edges{
     left,
     right,
     top,
-    down,
+    bottom
+}
+export enum Corners{
+    topLeft,
+    topRight,
+    bottomLeft,
+    bottomRight
 }
 class Position {
     public position: number;
 
     public size: number;
 
-    public static ErrorMessages: typeof ErrorsMessages;
-
     public constructor(position: number, size: number) {
-        conditionError(position >= 1 && position <= size * size, ErrorsMessages.positionError);
+        conditionError(position >= 1 && position <= size * size, ErrorMessages.positionError);
+        conditionError(size > 1, ErrorMessages.sizeError);
         this.position = position;
         this.size = size;
     }
 
-    public isEdge(direction: Directions): boolean {
-        if (direction === Directions.top) {
+    public isEdge(edge: Edges): boolean {
+        if (edge === Edges.top) {
             return this.position >= 1 && this.position <= this.size;
         }
-        return true;
+        if (edge === Edges.right) {
+            return this.position % this.size === 0;
+        }
+        if (edge === Edges.left) {
+            return this.position % this.size === 1;
+        }
+        if (edge === Edges.bottom) {
+            return ((this.size - 1) * this.size) < this.position && this.position <= this.size ** 2;
+        }
+        return false;
+    }
+
+    public hasEdge(edge: Edges): boolean {
+        return !this.isEdge(edge);
+    }
+
+    public hasCorner(corner: Corners): boolean {
+        if (corner === Corners.topLeft) {
+            return !(this.isEdge(Edges.top) || this.isEdge(Edges.left));
+        }
+        if (corner === Corners.topRight) {
+            return !(this.isEdge(Edges.top) || this.isEdge(Edges.right));
+        }
+        if (corner === Corners.bottomLeft) {
+            return !(this.isEdge(Edges.bottom) || this.isEdge(Edges.left));
+        }
+        if (corner === Corners.bottomRight) {
+            return !(this.isEdge(Edges.bottom) || this.isEdge(Edges.right));
+        }
+
+        return false;
     }
 }
-Position.ErrorMessages = ErrorsMessages;
 export default Position;
