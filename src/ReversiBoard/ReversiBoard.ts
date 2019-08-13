dc32db3d5a29887eeb1a0dea211875b1a406d53e
Manick <manick@Manicks-MacBook-Air.local>
2019-08-13 12:35:29 +0530

    change property accesor

---
+++ src/ReversiBoard/ReversiBoard.ts
@@ -0,0 +1,64 @@
+import Disk from '../Disk/Disk';
+import Position from '../Position/Position';
+import conditionError from '../utils/conditionError';
+
+const { DiskType } = Disk;
+const { ErrorMessages } = Position;
+
+class ReversiBoard {
+    private _size: number;
+
+    public get size(): number {
+        return this._size;
+    }
+
+    public set size(value: number) {
+        this._size = value;
+    }
+
+    private _disks: Disk[];
+
+    public get disks(): Disk[] {
+        return this._disks;
+    }
+
+    public set disks(value: Disk[]) {
+        this._disks = value;
+    }
+
+    public constructor(size: number) {
+        conditionError(size > 1, ErrorMessages.sizeError);
+        this._size = size;
+        this._disks = Array.from(
+            { length: (size * size) },
+            (_value, index): Disk => new Disk({
+                position: new Position({
+                    position: index + 1,
+                    size,
+                }),
+            }),
+        );
+    }
+
+    public getDisk(index: number): Disk {
+        return this._disks[index - 1];
+    }
+
+    public get boardSize(): number {
+        return this._size * this._size;
+    }
+
+    public printDisks(): void{
+        let result = '';
+        for (let i = 0; i < this._disks.length; i += this._size) {
+            for (let j = i; j <= i + this._size - 1; j += 1) {
+                result += `${this._disks[j].type}  `;
+            }
+            // eslint-disable-next-line no-console
+            console.log(`${result}\n`);
+            result = '';
+        }
+    }
+}
+
+export default ReversiBoard;
